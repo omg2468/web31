90f196be7b0904e3e239fe821262b48f5da3c189
omg2468 <115636091+omg2468@users.noreply.github.com>
2022-11-24 18:36:58 +0700

    update

---
+++ day9/test.js
@@ -0,0 +1,24 @@
+// Phép ++ và tương tự cho --
+let a = 1,
+  b = 1,
+  c = 1,
+  d = 1;
+a++;
+console.log(a); //--> a = 2
+++b;
+console.log(b); //--> b = 2
+
+//Cần lưu ý vị trí đặt toán tử ++ và -- đấy nhé
+console.log(c++); //--> c = 1
+console.log(c); //--> c = 2
+//---------------------------
+console.log(++d); //--> d = 2
+
+//Phép lũy thừa
+let e = 2;
+console.log(a ** 2); //--> 4
+
+//Phép / và %
+let f = 5;
+console.log(f / 2); //--> 2.5
+console.log(f % 2); //--> 1 | 0.5 ~ 1
