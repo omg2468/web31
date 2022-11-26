03cf04fb4fb2a2a91a9fb3d1e8e537752df1eb62
omg2468 <115636091+omg2468@users.noreply.github.com>
2022-11-26 15:55:57 +0700

    day7/bai1

---
+++ day9/bai2.js
@@ -0,0 +1,30 @@
+
+
+let y = 1;
+while (y <=6) {
+    y++; console.log(y)
+}
+
+let x = 1
+
+switch (x) {
+    case 2:
+        console.log(2)
+        break;
+    case 1: 
+    console.log(1)
+    break;
+    case 3:
+    console.log(3)
+    break;
+    default:
+    console.log(`chua nhap gi`)
+}
+
+try {
+    alertt('ok')
+
+}
+catch {
+    alert('lỗi rồi')
+}
