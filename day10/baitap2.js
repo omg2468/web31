f3fb61dce0e92e792c9f211680f903bb2b3e3626
omg2468 <115636091+omg2468@users.noreply.github.com>
2022-11-29 21:12:01 +0700

    update

--- day10/baitap2.js
+++ day10/baitap2.js
@@ -1,34 +1,159 @@
 /**
- * Tính số lần gấp 1 tờ giấy có độ mỏng 0.1mm để đạt được độ dày mong muốn
+ * Tìm số lớn nhất trong 3 số
  *
- * @param {number} thickness Độ dày mong muốn tính theo đơn vị m
+ * @param {number} a Số thứ 1
+ * @param {number} b Số thứ 2
+ * @param {number} c Số thứ 3
  *
- * @return {number} Số lần gấp giấy để đạt được độ dày yêu cầu
+ * @return {number} Số lớn nhất trong 3 số
  */
-
-/**function countFolding(thickness) {
-  let c = 0;
-  for (let a = 0.1; a <= (thickness*1000); a * 2) {
-    c++;
-  }
-  return console.log(c);
+function maxOfThree(a, b, c) {
+  if (a >= b && a >= c) return a;
+  else if (b >= a && b >= c) return b;
+  else return c;
 }
 
-countFolding(1);*/
+console.log(maxOfThree(10, 100, 100));
 
 /**
- * Tính xem sau bao nhiêu năm nữa thì tuổi cha gấp 2 lần tuổi con
+ * Tìm mùa tương ứng với tháng
  *
- * Tuổi cha lớn hơn tuổi con ít nhất 18 tuổi
+ * - Winter (mùa đông): 12, 1, 2
+ * - Spring (mùa xuân): 3, 4, 5
+ * - Summer (mùa hạ): 6, 7, 8
+ * - Fall (mùa thu): 9, 10, 11
  *
- * @param {number} dad Tuổi của cha
- * @param {number} son Tuổi của con
+ * @param {number} month Tháng trong năm
  *
- * @returns {number} Số năm để tuổi cha gấp 2 lần tuổi con
+ * @return {'Winter' | 'Spring' | 'Summer' | 'Fall'} Mùa tương ứng với tháng
  */
-function countYears(dad, son) {
-  
+function findSeason(month) {
+  switch (month) {
+    case 12:
+    case 1:
+    case 2:
+      return "winter";
+      break;
+    case 3:
+    case 4:
+    case 5:
+      return "spring";
+      break;
+    case 6:
+    case 7:
+    case 8:
+      return "summer";
+      break;
+    default:
+      return "Fall";
+  }
 }
 
-function countLegs() {
+console.log(findSeason(6));
+/**
+ * Kiểm tra xem một năm có phải năm nhuận hay không
+ *
+ * Theo lịch Gregory, năm nhuận là năm có số năm chia hết cho `4` và không chia hết cho `100`,
+ * tuy nhiên ngoại lệ với các năm chia hết cho `100` vẫn được coi là năm nhuận nếu nó cũng chia hết cho `400`
+ *
+ * - `2020` là năm nhuận vì chia hết cho `4` nhưng không chia hết cho `100`
+ * - `2100` không phải năm nhuận vì chia hết cho `4` nhưng chia hết cho `100`
+ * - `2000` là năm nhuận vì chia hết cho `100` nhưng cũng chia hết cho `400`
+ *
+ * @param {number} year Năm bất kỳ
+ *
+ * @return {boolean} Năm này có phải năm nhuận hay không
+ */
+function isLeafYear(year) {
+  return ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0));
 }
+
+console.log(isLeafYear(2022));
+/**
+ * Tìm số ngày trong tháng
+ *
+ * - Các tháng `1`, `3`, `5`, `7`, `8`, `10`, `12` có `31` ngày
+ * - Các tháng `4`, `6`, `9`, `11` có `30` ngày
+ * - Tháng `2` có `29` ngày nếu là năm nhuận, nếu không là `28` ngày
+ *
+ * @param {number} month Tháng bất kỳ
+ * @param {number} year Năm bất kỳ
+ *
+ * @return {number} Số ngày trong tháng đó
+ */
+function findDayOfMonth(month, year) {
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+    return 31;
+    break;
+    case 4:
+    case 6: 
+    case 9:
+    case 11:
+    return 30;
+    break;
+    case 2: 
+    return (isLeafYear(year)) ? 29 : 28;
+    break;
+    default: 
+    return 0
+  }
+}
+
+console.log(findDayOfMonth(2, 2021));
+/**
+ * Xếp loại sinh viên theo số điểm trung bình
+ *
+ * - Nếu điểm trung bình < 4.0 thì xếp loại 'F'
+ * - Nếu điểm trung bình < 5.5 thì xếp loại 'D'
+ * - Nếu điểm trung bình < 7.0 thì xếp loại 'C'
+ * - Nếu điểm trung bình < 8.5 thì xếp loại 'B'
+ * - Nếu điểm trung bình từ 8.5 đến 10 thì xếp loại 'A'
+ *
+ * @param {number} point Điểm trung bình của sinh viên
+ *
+ * @return {'A' | 'B' | 'C' | 'D' | 'F'} Xếp hạng tốt nghiệp của sinh viên
+ */
+
+/**
+ * Tính tiền cước taxi cho khách theo số kilomet đã di chuyển
+ *
+ * - Phí mở cửa cố định là `10.000đ`
+ * - Giá cho `30km` đầu tiên là `11.000đ/km`
+ * - Từ kilomet thứ `31` giá là `9.500đ/km`
+ * - Tổng tiền cước = Phí mở cửa + tổng tiền theo số kilomet đã di chuyển
+ *
+ * @param {number} km Số kilomet đã di chuyển
+ *
+ * @return {number} Số tiền phải trả
+ */
+function calcTaxiFee(km) {
+  let total = 0;
+  if (km <= 30) {total = 10 + 11*km}
+  else {total = 10 + 11*30 + 9.5*(km-30)}
+  return total
+}
+
+console.log(calcTaxiFee(39));
+
+/**
+ * Tìm nghiệm phương trình bậc 2 ax^2 + bx + c = 0
+ *
+ * - Nếu phương trình có vô số nghiệm thì trả về `Infinity`
+ * - Nếu phương trình vô nghiệm thì trả về `null`
+ * - Nếu phương trình có 1 nghiệm thì trả về nghiệm đó
+ * - Nếu phương trình có 2 nghiệm thì trả về một *mảng* chứa 2 nghiệm, cú pháp `[x1, x2]`
+ *
+ * @param {number} a Tham số a
+ * @param {number} b Tham số b
+ * @param {number} c Tham số c
+ *
+ * @return {number | [number, number] | null} Nghiệm phương trình
+ */
+function solveEquation(a, b, c) {}
---
+++ day10/baitap3.js
@@ -0,0 +1,95 @@
+/**
+ * Kiểm tra số một số nguyên dương bất kỳ có phải số nguyên tố hay không
+ *
+ * Số nguyên tố là số chỉ chia hết cho `1` và chính nó
+ *
+ * @param {number} n Số nguyên dương bất kỳ
+ *
+ * @return {boolean} Số `n` cho có phải số nguyên tố hay không
+ */
+function isPrime(n) {
+  for (x = 2; x < n; x++) {
+    if (n % x === 0) return false;
+  }
+  return true;
+}
+
+console.log(isPrime(9));
+
+/**
+ * Tính tổng các số nguyên tố trong khoảng từ 1 đến n
+ *
+ * @param {number} n Số nguyên dương bất kỳ
+ *
+ * @return {number} Tổng các số nguyên tố trong khoảng từ `1` đến `n`
+ */
+function sumOfPrimes(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+/**
+ * Tìm số Fibonacci thứ n
+ *
+ * Dãy Fibonacci là dãy số bắt đầu bằng 0 và 1, các số tiếp theo bằng tổng 2 số trước nó
+ *
+ * VD: 0 1 1 2 3 5 8 13 ... (Số thứ 1 là 0, số thứ 2 là 1, số thứ 3 là 2, ...)
+ *
+ * VD: `n = 5`, kết quả số Fibonacci thứ 5 là `3`
+ *
+ * @param {number} n Số nguyên dương bất kỳ (n > 0)
+ *
+ * @return {number} Số Fibonacci thứ `n`
+ */
+function fibonacci(n) {
+  let a = 0;
+  let b = 1;
+  let c;
+  if (n === 0) return a;
+  else if (n == 1) return b;
+  else {
+    for (let i = 2; i <= n; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return c;
+  }
+}
+
+/**
+ * Tính giai thừa của một số nguyên bất kỳ
+ *
+ * Giai thừa của một số `n = 1 * 2 * 3 * ... * (n - 1) * n`
+ *
+ * @param {number} n Số nguyên dương bất kỳ (n >= 0)
+ *
+ * @return {number} Giai thừa của `n`
+ */
+function factorial(n) {
+    let x = 1;
+    for (let i = 2; i < n; i++){
+        x = i*(i+1)
+    }
+    return x
+}
+console.log(factorial(5));
+
+
+/**
+ * Kiểm tra một số nguyên dương bất kỳ có phải số Strong hay không
+ *
+ * Số Strong là số có tổng giai thừa các chữ số bằng chính nó
+ *
+ * VD: 145 = 1! + 4! + 5! = 1 + 24 + 120 = 145
+ *
+ * @param {number} n Số nguyên dương bất kỳ
+ *
+ * @return {boolean} Số `n` có phải là số Strong hay không
+ */
+function isStrong(n) {}
