b77cea34d3d5349c40909a67a1b9303e2e93deab
omg2468 <115636091+omg2468@users.noreply.github.com>
2023-02-09 18:33:26 +0700

    update

--- day1/test.js
+++ day1/test.js
@@ -5,46 +5,106 @@
 // }).then((json) => {
 //     console.log(json)
 // })
-
-class BaseTeacherClass {
-    constructor(id, age, name){
-        this.id = id;
-        this.age = age;
-        this.name = name;
-    }
-
-    get getID(){
-        return this.id
-    }
-
-    get getAge(){
-        return this.age
-    }
-
-    get getName(){
-        return this.name
-    }
-}
-
-class FreelancerTeacher extends BaseTeacherClass {
+// Cách 1
+class BaseTeacherClass1 {
   constructor(id, age, name) {
-    super(id, age, name)
+    this.id = id;
+    this.age = age;
+    this.name = name;
+  }
+
+  get getID() {
+    return this.id;
+  }
+
+  get getAge() {
+    return this.age;
+  }
+
+  get getName() {
+    return this.name;
   }
 }
 
-class FulltimeTeacher extends BaseTeacherClass {
+class FreelancerTeacher1 extends BaseTeacherClass1 {
   constructor(id, age, name) {
-    super(id, age, name)
-  }
-}
- 
-class ParttimeTeacher extends BaseTeacherClass {
-  constructor(id, age, name) {
-    super(id, age, name)
+    super(id, age, name);
   }
 }
 
-let huong = new ParttimeTeacher(20901, 20, 'huong');
+class FulltimeTeacher1 extends BaseTeacherClass1 {
+  constructor(id, age, name) {
+    super(id, age, name);
+  }
+}
 
+class ParttimeTeacher1 extends BaseTeacherClass1 {
+  constructor(id, age, name) {
+    super(id, age, name);
+  }
+}
 
-console.log(huong.getID)
+// Cách 2
+
+class FreelancerTeacher2 {
+  constructor(id, age, name) {
+    this.id = id;
+    this.age = age;
+    this.name = name;
+  }
+  get getID() {
+    return this.id;
+  }
+
+  get getAge() {
+    return this.age;
+  }
+
+  get getName() {
+    return this.name;
+  }
+}
+
+class FulltimeTeacher2{
+  constructor(id, age, name) {
+    this.id = id;
+    this.age = age;
+    this.name = name;
+  }
+  get getID() {
+    return this.id;
+  }
+
+  get getAge() {
+    return this.age;
+  }
+
+  get getName() {
+    return this.name;
+  }
+}
+
+class ParttimeTeacher2 {
+  constructor(id, age, name) {
+    this.id = id;
+    this.age = age;
+    this.name = name;
+  }
+  get getID() {
+    return this.id;
+  }
+
+  get getAge() {
+    return this.age;
+  }
+
+  get getName() {
+    return this.name;
+  }
+}
+
+var huong = new ParttimeTeacher2(132, 20, "huong");
+var lu = new FulltimeTeacher2(12,30, 'lu');
+
+console.log(huong.getAge)
+console.log(lu.getAge)
