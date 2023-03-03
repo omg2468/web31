9c3a924e596018ff7d617325b2fb43f5f5a9ce59
omg2468 <115636091+omg2468@users.noreply.github.com>
2023-03-03 20:21:58 +0700

    3/3/2023

--- project/index.js
+++ project/index.js
@@ -1,17 +1,42 @@
-const navbar = document.querySelector(".navbar.navbar-expand-lg");
+const navbar = document.querySelector(".navbar_container");
+const navBarToogle = document.querySelector(".navbar-toggler");
+const navBarToogleIcon = document.querySelector(".navbar-toggler-icon");
+console.log(navBarToogleIcon.className);
 const aboutUs = document.querySelector(".container-fluid.about_us");
+const navBarLink = document.querySelectorAll(".nav-item a");
+const btnNav = document.querySelector(".navbar .container-fluid button span");
+const cart = document.querySelector(".bi-cart4");
+console.log(navBarLink);
 // navbar
 const scrollNav = () => {
   let aboutTop = aboutUs.getBoundingClientRect().top;
   if (aboutTop <= 0) {
-    navbar.style.backgroundColor = "rgb(119, 124, 106)";
-  } else navbar.style.backgroundColor = "rgba(119, 124, 106, 0.258)";
+    navbar.style.backgroundColor = "white";
+    navbar.style.color = "black";
+    navBarLink.forEach((link) => {
+      link.style.color = "black";
+    });
+    btnNav.style.color = "black";
+    cart.style.color = "black";
+    navBarToogle.style.borderColor = "black";
+    navBarToogleIcon.className = "navbar-toggler-icon black";
+  } else {
+    navbar.style.backgroundColor = "rgba(119, 124, 106, 0.258)";
+    navbar.style.color = "white";
+    navBarLink.forEach((link) => {
+      link.style.color = "white";
+    });
+    btnNav.style.color = "white";
+    cart.style.color = "white";
+    navBarToogle.style.borderColor = "white";
+    navBarToogleIcon.className = "navbar-toggler-icon white";
+  }
 };
 window.addEventListener("scroll", scrollNav);
 
-// swiper
+// swiper best seller
 
-var TrandingSlider = new Swiper(".swiper", {
+var TrandingSlider = new Swiper(".swiper1", {
   effect: "coverflow",
   centeredSlides: true,
   loop: true,
@@ -28,7 +53,7 @@ var TrandingSlider = new Swiper(".swiper", {
     },
   },
   coverflowEffect: {
-    rotate: 10,
+    rotate: 0,
     stretch: 0,
     depth: 200,
     modifier: 2,
@@ -36,5 +61,8 @@ var TrandingSlider = new Swiper(".swiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  autoplay: {
+    delay: 4000,
   },
 });
